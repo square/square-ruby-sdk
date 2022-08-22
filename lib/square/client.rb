4ebc53b9ef56d3605f6c3ebe27f9400a9f6dc115
autobot <devs-apex+github-pusher@squareup.com>
2022-08-22 22:21:29 +0000

    Generated PR for Release: 22.1.0.20220823

--- lib/square/client.rb
+++ lib/square/client.rb
@@ -4,7 +4,7 @@ module Square
     attr_reader :config
 
     def sdk_version
-      '22.0.0.20220817'
+      '22.1.0.20220823'
     end
 
     def square_version
@@ -242,7 +242,7 @@ module Square
                    retry_statuses: [408, 413, 429, 500, 502, 503, 504, 521, 522, 524],
                    retry_methods: %i[get put], environment: 'production',
                    custom_url: 'https://connect.squareup.com',
-                   square_version: '2022-08-17', access_token: '',
+                   square_version: '2022-08-23', access_token: '',
                    user_agent_detail: '', additional_headers: {}, config: nil)
       @config = if config.nil?
                   Configuration.new(connection: connection, adapter: adapter,
