17990f08e107662f707921e1814fe5611ccfdd99
jsklan <jsklan.development@gmail.com>
2025-08-14 00:01:39 -0400

    add more test files

---
+++ test/integration/test_cash_drawers.rb
@@ -0,0 +1,22 @@
+require_relative './helpers'
+require_relative './integration_test_base'
+
+
+class CashDrawersTest < IntegrationTestBase
+  def setup
+    setup_class
+  end
+
+  def test_list_cash_drawer_shifts
+    start_time = Time.now - 3600  # 1 hour ago
+    end_time = Time.now
+    
+    response = @client.cash_drawers.shifts.list(
+      location_id: get_default_location_id(@client),
+      begin_time: start_time.iso8601,
+      end_time: end_time.iso8601
+    )
+
+    refute_nil(response)
+  end
+end
