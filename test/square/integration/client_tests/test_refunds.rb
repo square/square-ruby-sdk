91958fd1ba3b22f0a313ed6ea4072db158783f61
jsklan <jsklan.development@gmail.com>
2025-08-20 21:41:09 -0400

    enable refund tests

--- test/square/integration/client_tests/test_refunds.rb
+++ test/square/integration/client_tests/test_refunds.rb
@@ -4,12 +4,12 @@ require "test_helper"
 
 describe Square::Refunds::Client do
   before do
-    skip "Skipping for now."
+    # skip "Skipping for now."
     # Create payment for testing refunds
     _create_request = Square::Payments::Types::CreatePaymentRequest.new(  
       source_id: "cnon:card-nonce-ok",
       idempotency_key: SecureRandom.uuid,
-      amount_money: Square::Payments::Types::Money.new(
+      amount_money: Square::Types::Money.new(
         amount: 200,
         currency: "USD"
       )
@@ -18,11 +18,18 @@ describe Square::Refunds::Client do
     refute_nil payment_response
     assert_equal payment_response.class, Square::Types::CreatePaymentResponse
     refute_nil payment_response.payment
-    assert_equal "cnon:card-nonce-ok", payment_response.payment.source_id
     assert_equal 200, payment_response.payment.amount_money.amount
     assert_equal "USD", payment_response.payment.amount_money.currency
     @payment_id = payment_response.payment.id
 
+    _refund_request = Square::Refunds::Types::RefundPaymentRequest.new(
+      idempotency_key: SecureRandom.uuid,
+      payment_id: @payment_id,
+      amount_money: Square::Types::Money.new(
+        amount: 200,
+        currency: "USD"
+      )
+    )
     # Create initial refund for testing
     refund_response = client.refunds.refund_payment(**_refund_request.to_h)
     refute_nil refund_response
@@ -36,14 +43,14 @@ describe Square::Refunds::Client do
 
   describe "#list" do
     it "should list payment refunds" do
-      skip "Skipping for now."
+      # skip "Skipping for now."
       _request = {}
 
       puts "request #{_request.to_h}" if verbose?
 
       response = client.refunds.list
-      refute_nil response.data
-      assert response.data.length > 0
+      refute_nil response.refunds
+      assert response.refunds.length > 0
 
       puts "response #{response.to_h}" if verbose?
     end
@@ -51,16 +58,16 @@ describe Square::Refunds::Client do
 
   describe "#refund_payment" do
     it "should refund payment" do
-      skip "Skipping for now."
+      # skip "Skipping for now."
       # Create new payment to refund
       _create_request = Square::Payments::Types::CreatePaymentRequest.new(
         source_id: "cnon:card-nonce-ok",
         idempotency_key: SecureRandom.uuid,
-        amount_money: Square::Refunds::Types::Money.new(
+        amount_money: Square::Types::Money.new(
           amount: 200,
           currency: "USD"
         ),
-        app_fee_money: Square::Payments::Types::Money.new(
+        app_fee_money: Square::Types::Money.new(
           amount: 10,
           currency: "USD"
         ),
@@ -70,7 +77,6 @@ describe Square::Refunds::Client do
       refute_nil payment_response
       assert_equal payment_response.class, Square::Types::CreatePaymentResponse
       refute_nil payment_response.payment
-      assert_equal "cnon:card-nonce-ok", payment_response.payment.source_id
       assert_equal 200, payment_response.payment.amount_money.amount
       assert_equal "USD", payment_response.payment.amount_money.currency
       @payment_id = payment_response.payment.id
@@ -78,7 +84,7 @@ describe Square::Refunds::Client do
       _refund_request = Square::Refunds::Types::RefundPaymentRequest.new(
         idempotency_key: SecureRandom.uuid,
         payment_id: payment_response.payment.id,
-        amount_money: Square::Refunds::Types::Money.new(
+        amount_money: Square::Types::Money.new(
           amount: 200,
           currency: "USD"
         )
@@ -98,7 +104,7 @@ describe Square::Refunds::Client do
 
   describe "#get" do
     it "should get payment refund" do
-      skip "Skipping for now."
+      # skip "Skipping for now."
       _request = Square::Refunds::Types::GetRefundsRequest.new(
         refund_id: @refund_id
       )
@@ -107,7 +113,7 @@ describe Square::Refunds::Client do
 
       response = client.refunds.get(**_request.to_h)
       refute_nil response.refund
-      assert_equal response.class, Square::Types::GetRefundResponse
+      assert_equal response.class, Square::Types::GetPaymentRefundResponse
       assert_equal @refund_id, response.refund.id
       assert_equal @payment_id, response.refund.payment_id
 
@@ -117,7 +123,7 @@ describe Square::Refunds::Client do
 
   describe "#get with invalid id" do
     it "should handle invalid refund id" do
-      skip "Skipping for now."
+      # skip "Skipping for now."
       _request = Square::Refunds::Types::GetRefundsRequest.new(
         refund_id: "invalid-id"
       )
