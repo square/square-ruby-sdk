a220cade1780ff4665c390580dfe1885a7fa36ba
Chris Hanks <christopher.m.hanks@gmail.com>
2025-09-17 16:27:36 -0400

    Add docstrings to the ItemIterator and PageIterator classes.

--- lib/square/internal/iterators/item_iterator.rb
+++ lib/square/internal/iterators/item_iterator.rb
@@ -3,22 +3,37 @@ module Square
     class ItemIterator
       include Enumerable
 
+      # Instantiates a ItemIterator, an Enumerable class which wraps calls to a paginated API and yields the individual items from the API.
+      #
+      # @param initial_cursor [String] The initial cursor to use when iterating.
+      # @return [Square::Internal::ItemIterator]
       def initialize(initial_cursor:, item_field:, &block)
         @item_field = item_field
         @page_iterator = PageIterator.new(initial_cursor:, &block)
         @page = nil
       end
 
+      # Returns the PageIterator mediating access to the underlying API.
+      #
+      # @param initial_cursor [String] The initial cursor to use when iterating.
+      # @return [Square::Internal::PageIterator]
       def pages
         @page_iterator
       end
 
+      # Iterates over each item returned by the API.
+      #
+      # @param block [Proc] The block which is passed every page as it is received.
+      # @return [nil]
       def each(&block)
         while item = get_next do
           block.call(item)
         end
       end
 
+      # Whether another item will be available from the API.
+      #
+      # @return [Boolean]
       def has_next?
         load_next_page if @page.nil?
         return false if @page.nil?
@@ -28,6 +43,9 @@ module Square
         any_items_in_cached_page
       end
 
+      # Retrieves the next item from the API.
+      #
+      # @return [Boolean]
       def get_next
         item = next_item_from_cached_page
         return item if item
--- lib/square/internal/iterators/page_iterator.rb
+++ lib/square/internal/iterators/page_iterator.rb
@@ -3,21 +3,35 @@ module Square
     class PageIterator
       include Enumerable
 
+      # Instantiates a PageIterator, an Enumerable class which wraps calls to a paginated API and yields pages of items.
+      #
+      # @param initial_cursor [String] The initial cursor to use when iterating.
+      # @return [Square::Internal::PageIterator]
       def initialize(initial_cursor:, &block)
         @cursor = initial_cursor
         @get_next_page = block
       end
 
+      # Iterates over each page returned by the API.
+      #
+      # @param block [Proc] The block which is passed every page as it is received.
+      # @return [nil]
       def each(&block)
         while @cursor do
           block.call(get_next)
         end
       end
 
+      # Whether another page will be available from the API.
+      #
+      # @return [Boolean]
       def has_next?
         !@cursor.nil?
       end
 
+      # Retrieves the next page from the API.
+      #
+      # @return [Boolean]
       def get_next
         return if @cursor.nil?
         next_page = @get_next_page.call(@cursor)
