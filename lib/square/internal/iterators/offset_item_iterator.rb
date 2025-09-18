8bbb3076492ebc99b393556e969017f1c6f6fe43
Chris Hanks <christopher.m.hanks@gmail.com>
2025-09-18 15:31:42 -0400

    Merge common logic into an ItemIterator class.

--- lib/square/internal/iterators/offset_item_iterator.rb
+++ lib/square/internal/iterators/offset_item_iterator.rb
@@ -1,8 +1,6 @@
 module Square
   module Internal
-    class OffsetItemIterator
-      include Enumerable
-
+    class OffsetItemIterator < ItemIterator
       # Instantiates an OffsetItemIterator, an Enumerable class which wraps calls to an offset-based paginated API and yields the individual items from it.
       #
       # @param initial_cursor [String] The initial cursor to use when iterating.
@@ -19,54 +17,6 @@ module Square
       # @return [Square::Internal::OffsetPageIterator]
       def pages
         @page_iterator
-      end
-
-      # Iterates over each item returned by the API.
-      #
-      # @param block [Proc] The block which each retrieved item is yielded to.
-      # @return [nil]
-      def each(&block)
-        while item = get_next do
-          block.call(item)
-        end
-      end
-
-      # Whether another item will be available from the API.
-      #
-      # @return [Boolean]
-      def has_next?
-        load_next_page if @page.nil?
-        return false if @page.nil?
-
-        return true if any_items_in_cached_page
-        load_next_page
-        any_items_in_cached_page
-      end
-
-      # Retrieves the next item from the API.
-      #
-      # @return [Boolean]
-      def get_next
-        item = next_item_from_cached_page
-        return item if item
-        load_next_page
-        next_item_from_cached_page
-      end
-
-      private
-
-      def next_item_from_cached_page
-        return if !@page
-        @page.send(@item_field).shift
-      end
-
-      def any_items_in_cached_page
-        return false if !@page
-        !@page.send(@item_field).empty?
-      end
-
-      def load_next_page
-        @page = @page_iterator.get_next
       end
     end
   end
